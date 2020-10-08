1931f3297e9af473c2610cd6c5ac28f179591958
rhys <email.curly@gmail.com>
2020-10-08 17:46:50 +0100

    Using ps now

--- procs.c
+++ procs.c
@@ -9,7 +9,11 @@
 #define PROC "/proc"
 
 typedef struct Proc{
-    unsigned int id;
+    char* user;
+    unsigned int pid;
+    unsigned int cpu_perc;
+    unsigned int mem_perc;
+    char* command;
 } proc;
 
 typedef struct Proc_List{
@@ -27,51 +31,82 @@ int isnumber(const char* to_test){
     return 1;
 }
 
+proc split_ps(char* in){
+    char *token = strtok(in, " \t");
+    int i = 0;
 
+    printf("\n\n\n");
 
-proc_list get_proc_nums(){
-    DIR *dir;
-    struct dirent *entry;
+    proc out;
+
+    while (token) {
+        if(i == 0){
+            out.user = calloc(strlen(token), sizeof(char));
+            strcpy(out.user, token);
+        }if(i == 1){
+            out.pid = atoi(token);
+        }else if(i == 2){
+            out.cpu_perc = atof(token);
+        }else if(i == 3){
+            out.mem_perc = atof(token);
+        }else if(i == 10){
+            int len = strlen(token);
+            out.command = calloc(len, sizeof(char));
+            strcpy(out.command, token);
+
+            if(out.command[len - 1] == '\n') {
+                out.command[len - 1] = 0;
+            }
+            break;
+        }
+
+        if(i != 9) {
+            token = strtok(NULL, " \t");
+        }else{
+            token = strtok(NULL, "");
+        }
+
+        i++;
+    }
+
+    return out;
+}
+
+proc_list read_procs(){
+    FILE *fp;
+    char path[1035];
 
     proc_list out = {
             0,
             NULL
     };
 
-    if (!(dir = opendir(PROC)))
-        return out;
-
-    while ((entry = readdir(dir)) != NULL) {
-
-        if (entry->d_type == DT_DIR && isnumber(entry->d_name)){
-            if ((strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) && !isnumber(entry->d_name)) {
-                continue;
-            }
-
-            proc p = {
-                    atoi(entry->d_name)
-            };
-
-            out.length++;
-            if(out.list == NULL){
-                out.list = malloc(out.length*sizeof(proc));
-            }else{
-                out.list = realloc(out.list, out.length*sizeof(proc));
-            }
-
-            out.list[out.length-1] = p;
-
-        }
+    fp = popen("/usr/bin/ps aux --sort=-pcpu", "r");
+    if (fp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
     }
-    closedir(dir);
+
+    int line = 0;
+    while (fgets(path, sizeof(path), fp) != NULL) {
+        if(line == 0){ line++; continue;}
+
+        proc p = split_ps(path);
+
+        printf("User: %s\n", p.user);
+
+        out.length++;
+
+        if(out.length == 1){
+            out.list = malloc(sizeof(p));
+        }else{
+            out.list = realloc(out.list, (sizeof(out.list)+sizeof(p)));
+        }
+
+        out.list[out.length] = p;
+    }
+
+    pclose(fp);
 
     return out;
-}
-
-void read_procs(){
-    proc_list procs = get_proc_nums();
-
-    for(int i=0 ; i<procs.length ; i++){
-        printf("(%d) Proc %d\n", i, procs.list[i].id);
-    }
 }
